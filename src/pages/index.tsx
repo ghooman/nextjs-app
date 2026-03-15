39aa0abee2a40de6b6af564443548cc504a584b1
ghoo <ghoo@ghoo-MacBook-Air.local>
2026-03-16 00:50:14 +0900

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+
+import homeStyles from "@/styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Jihoo Park</title>
+      </Head>
+      <section className={homeStyles.headingMd}>
+        <p>[Jihoo Park Introduction]</p>
+        <p>(This is a website)</p>
+      </section>
+      <section className={`${homeStyles.headingMd} ${homeStyles.padding1px}`}>
+        <h2 className={homeStyles.headingLg}>Blog</h2>
+        <ul className={homeStyles.list}></ul>
+      </section>
+    </>
+  );
+}
